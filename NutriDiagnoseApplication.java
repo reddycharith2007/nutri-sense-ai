44921af415285a2b6dde73bc1dfbb4d9b5ea7a4a
C REDDI CHARITH REDDY <reddycharith2007@gmail.com>
2025-10-15 20:36:41 +0530

    Create NutriDiagnoseApplication.java

---
+++ NutriDiagnoseApplication.java
@@ -0,0 +1,11 @@
+package com.nutri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriDiagnoseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NutriDiagnoseApplication.class, args);
+    }
+}
