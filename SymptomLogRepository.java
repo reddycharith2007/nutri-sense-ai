173b62ef7143db9b9cc05e0596eb22b2d90a2fab
C REDDI CHARITH REDDY <reddycharith2007@gmail.com>
2025-10-15 20:38:03 +0530

    Create SymptomLogRepository.java

---
+++ SymptomLogRepository.java
@@ -0,0 +1,6 @@
+package com.nutri.repository;
+
+import com.nutri.entity.SymptomLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SymptomLogRepository extends JpaRepository<SymptomLog, Long> {}
